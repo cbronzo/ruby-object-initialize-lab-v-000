78b3aabfd3fdb1be1701dbf73591df9af79f5b18
Catherine Bronzo <cbronzo@mac.com>
2018-06-05 23:09:55 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,5 @@
 class Person
-  def intialize(name = "Beyonce")
+  def initialize(name)
     @name = name
-  end
-  def name=(name)
-    @name = name
-  end
- 
-  def name
-    @name
   end
 end
-
-beyonce = Person.new
-beyonce.name
